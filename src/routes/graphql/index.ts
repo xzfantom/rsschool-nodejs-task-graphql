b2701c7f2c7275f3dfb5c0c7f7386f9d1b9fd9f1
Artsiom Martynau <xzfantom@gmail.com>
2023-07-22 16:47:12 +0200

    feat: main point for data request

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLSchema, graphql, parse, validate } from 'graphql';
+import { Query } from './types/query.js';
+import { Mutation } from './types/mutation.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +16,29 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const schema = new GraphQLSchema({
+        query: Query,
+        mutation: Mutation,
+      });
+
+      const queryBody = req.body.query;
+      const variablesBody = req.body.variables;
+
+      const errors = validate(schema, parse(queryBody), [depthLimit(5)]);
+      if (errors.length > 0) {
+        return {
+          errors,
+        };
+      }
+
+      console.log('queryBody:', queryBody, variablesBody);
+
+      return await graphql({
+        schema,
+        source: queryBody,
+        variableValues: variablesBody,
+        contextValue: fastify,
+      });
     },
   });
 };
